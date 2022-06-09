b7210b8a202fee18132fab4ac304b38c0caba243
Darice Sousa <darice.sousa@gmail.com>
2022-06-09 16:41:50 -0300

    configurando o tema

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/modules/splash/splash_page.dart';
 
 class AppModule extends Module{
   @override
@@ -7,5 +8,7 @@ class AppModule extends Module{
 
   @override
   // TODO: implement routes
-  List<ModularRoute> get routes => [];
+  List<ModularRoute> get routes => [
+    ChildRoute('/', child: (context, args) => const SplashPage())
+  ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/app_module.dart';
+import 'package:job_timer/app/app_widget.dart';
 import 'package:job_timer/firebase_options.dart';
 
 void main() async {
@@ -7,4 +10,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+    runApp(ModularApp(
+    module: AppModule(),
+    child: const AppWidget(),
+  ));
+
 }
