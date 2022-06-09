250c25fd0e288e2f8b8702cae47e162a214336eb
Darice Sousa <darice.sousa@gmail.com>
2022-06-09 16:28:19 -0300

    inseri modular e asuka

---
+++ lib/app/app_module.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module{
+  @override
+  // TODO: implement binds
+  List<Bind> get binds  => [];
+
+  @override
+  // TODO: implement routes
+  List<ModularRoute> get routes => [];
+}
