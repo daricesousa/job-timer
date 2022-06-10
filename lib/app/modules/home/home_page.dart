088513a0d1dddaa16a89e82839ecf4cbffaaceaf
Darice Sousa <darice.sousa@gmail.com>
2022-06-10 14:39:35 -0300

    adicionei interceptor do firebase-auth

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text(''),),
+           appBar: AppBar(title: const Text('Home'),),
            body: Container(),
        );
   }
