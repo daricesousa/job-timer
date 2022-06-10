48fd7a1e605d36864e2ce8feb1ef62c5b2531c7f
Darice Sousa <darice.sousa@gmail.com>
2022-06-10 14:23:23 -0300

    página de splash

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:job_timer/app/core/ui/job_timer_icons.dart';
 
 class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
 
-  const SplashPage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Color(0XFF0092B9),
+            Color(0XFF0167B2),
+          ],
+        )),
+        child: Center(
+            child: Image.asset(
+          "assets/images/logo.png",
+        )),
+      ),
+    );
   }
 }
