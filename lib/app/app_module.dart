14effb3fd7bccfc703173e515b292695a349764f
Darice Sousa <darice.sousa@gmail.com>
2022-06-10 15:31:31 -0300

    autenticação com o google

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,14 +2,16 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/modules/home/home_module.dart';
 import 'package:job_timer/app/modules/login/login_module.dart';
 import 'package:job_timer/app/modules/splash/splash_page.dart';
+import 'package:job_timer/app/services/auth/auth_service.dart';
+import 'package:job_timer/app/services/auth/auth_service_impl.dart';
 
 class AppModule extends Module{
   @override
-  // TODO: implement binds
-  List<Bind> get binds  => [];
+  List<Bind> get binds  => [
+    Bind.lazySingleton<AuthService>((i) => AuthServiceImpl())
+  ];
 
   @override
-  // TODO: implement routes
   List<ModularRoute> get routes => [
     ChildRoute('/', child: (context, args) => const SplashPage()),
     ModuleRoute('/login', module: LoginModule()),
